b58c2644391248679e429b11a4415f3bd914e6f4
devblok <l.praninskas@gmail.com>
2020-12-21 00:32:19 +0200

    Progress on the router, does not compile

--- src/router.zig
+++ src/router.zig
@@ -4,6 +4,7 @@
 // https://opensource.org/licenses/MIT
 
 const std = @import("std");
+const os = std.os;
 const Allocator = std.mem.Allocator;
 
 /// The definition on an IP packet header.
@@ -30,26 +31,85 @@ pub const PacketHeader = packed struct {
 // 6. Repeat.
 // Misc. Allow insertion and deletion of clients from the loop
 
+const Error = error{Interrupted};
+
+pub const HandlerFn = fn () void;
+
+pub const Handler = struct {
+    fd: i32,
+    func: HandlerFn,
+};
+
 /// Implements the packet handling mechanism.
 pub const Router = struct {
+    epoll_fd: i32,
+    epoll_timeout: i32,
+    max_concurrent: u32,
+
     allocator: *Allocator,
-    sockets: std.ArrayList(i32),
+    handlers_lock: std.Mutex,
+    handlers: std.AutoHashMap(i32, Handler),
 
     const Self = @This();
 
     /// Starts processing the incoming packets and writing them.
     pub fn run(self: Self) !void {
-        return;
+        const events = try self.allocator.alloc(os.epoll_event, self.max_concurrent);
+        defer self.allocator.free(events);
+
+        try self.epoll_proc(events);
     }
 
-    pub fn init(allocator: *Allocator) !Self {
+    pub fn register(self: Self, handler: Handler) !void {
+        const lock = self.handlers_lock.acquire();
+        defer lock.release();
+
+        try self.handlers.put(handler.fd, handler);
+    }
+
+    pub fn init(allocator: *Allocator, max_concurrent: u32, epoll_timeout: i32) !Self {
         return Self{
+            .epoll_fd = try os.epoll_create1(0),
+            .epoll_timeout = epoll_timeout,
+            .max_concurrent = max_concurrent,
             .allocator = allocator,
-            .sockets = std.ArrayList(i32).init(allocator),
+            .handlers = std.AutoHashMap(i32, Handler).init(allocator),
+            .handlers_lock = std.Mutex{},
         };
     }
 
     pub fn deinit(self: Self) void {
-        self.sockets.deinit();
+        os.close(self.epoll_fd);
+        self.handlers.deinit();
+    }
+
+    fn epoll_proc(self: Self, events: []os.epoll_event) Error!void {
+        var ret: usize = 0;
+        while (ret >= 0) : (ret = os.epoll_wait(self.epoll_fd, events.ptr, events.len, self.epoll_timeout)) {
+            for (events[0..ret]) |event| {
+                const lock = self.handlers_lock.acquire();
+
+                if (self.handlers.get(event.fd)) |handler| {
+                    handler.func();
+                }
+
+                lock.release();
+            }
+        }
+        return;
     }
 };
+
+fn print() !void {
+    std.debug.print("epoll handled\n", .{});
+}
+
+test "epoll detection" {
+    var router = Router.init(std.heap.page_allocator, 1, 100);
+    const pipes = try os.pipe();
+
+    try router.register(.{
+        .fd = pipes[1],
+        .func = print,
+    });
+}
